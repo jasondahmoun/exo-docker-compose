5200fba2c648a99fd89e9488060476737be0d7eb
Jason-kosmos <jason@naxan-group.com>
2026-01-20 14:16:00 +0100

    exo 1

---
+++ exo1/backend/back.py
@@ -0,0 +1,11 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.get("/api/hello")
+def hello():
+    return jsonify(message="Hello World")
+
+app.run(host="0.0.0.0", port=5000)
